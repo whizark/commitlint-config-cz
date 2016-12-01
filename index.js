18645c4adc5c9d7603413e30ddd01ffba74096d2
Whizark <contact@whizark.com>
2016-12-02 00:21:30 +0900

    add config files

--- index.js
+++ index.js
@@ -1,3 +1,155 @@
 'use strict';
 
-module.exports = {};
+const types  = require('./lib/types');
+const scopes = require('./lib/scopes');
+
+module.exports = {
+    rules    : {
+        'body-leading-blank'  : [
+            2,
+            'always',
+        ],
+        'body-max-length'     : [
+            1,
+            'always',
+            Infinity,
+        ],
+        'body-min-length'     : [
+            1,
+            'always',
+            0,
+        ],
+        'body-tense'          : [
+            2,
+            'always',
+            [
+                'present-imperative',
+            ],
+        ],
+        'footer-leading-blank': [
+            2,
+            'always',
+        ],
+        'footer-max-length'   : [
+            1,
+            'always',
+            Infinity,
+        ],
+        'footer-min-length'   : [
+            1,
+            'always',
+            0,
+        ],
+        'footer-tense'        : [
+            2,
+            'always',
+            [
+                'present-imperative',
+            ],
+        ],
+        'header-max-length'   : [
+            1,
+            'always',
+            72,
+        ],
+        'header-min-length'   : [
+            1,
+            'always',
+            0,
+        ],
+        'lang'                : [
+            1,
+            'always',
+            'eng',
+        ],
+        'scope-enum'          : [
+            2,
+            'always',
+            scopes(),
+        ],
+        'scope-case'          : [
+            2,
+            'always',
+            'lowerCase',
+        ],
+        'scope-empty'         : [
+            1,
+            'never',
+        ],
+        'scope-max-length'    : [
+            1,
+            'always',
+            Infinity,
+        ],
+        'scope-min-length'    : [
+            1,
+            'always',
+            0,
+        ],
+        'subject-case'        : [
+            2,
+            'always',
+            'lowerCase',
+        ],
+        'subject-empty'       : [
+            2,
+            'never',
+        ],
+        'subject-full-stop'   : [
+            2,
+            'never',
+        ],
+        'subject-max-length'  : [
+            1,
+            'always',
+            Infinity,
+        ],
+        'subject-min-length'  : [
+            1,
+            'always',
+            0,
+        ],
+        'subject-tense'       : [
+            2,
+            'always',
+            [
+                'present-imperative',
+            ],
+        ],
+        'type-enum'           : [
+            2,
+            'always',
+            types(),
+        ],
+        'type-case'           : [
+            2,
+            'always',
+            'lowerCase',
+        ],
+        'type-empty'          : [
+            2,
+            'never',
+        ],
+        'type-max-length'     : [
+            1,
+            'always',
+            Infinity,
+        ],
+        'type-min-length'     : [
+            1,
+            'always',
+            0,
+        ],
+    },
+    wildcards: {
+        merge  : [
+            '/^(Merge pull request)|(Merge (.*?) into (.*?)$)/',
+        ],
+        release: [
+            '/^\\d+.\\d+.\\d+$/',
+        ],
+        revert : [
+            '/^revert: (.*)/',
+        ],
+    },
+};
